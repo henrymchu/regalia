e66edf6dd5009d8259eeb73ea6913533eed23049
Henry Chu <collettemilton@Collettes-MBP.T-mobile.com>
2022-10-26 20:40:31 -0700

    Add input and data acquisition notes

--- data_acquisition.py
+++ data_acquisition.py
@@ -1,1 +1,12 @@
 # Module for acquiring data from coinmarketcap/coingecko APIs
+
+# What websites have API access to get data?
+#     1. Are there rate limits to how often we can hit the service for information?
+
+# What types of APIs are available to get ...
+#     1. List of all assets available at a specific exchange
+#     2. List of all exchanges that support a specific asset or trading pair
+
+# Notes
+#     1. https://www.coingecko.com/en/api
+#     2. https://coinmarketcap.com/api/
