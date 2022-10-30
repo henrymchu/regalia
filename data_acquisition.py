896d34d23f16f3d973b6a6c010e7b57feac3092b
Matthew Lee <matthewlee44@gmail.com>
2022-10-30 18:21:44 -0500

    code to start exploring how to use coin market cap API

--- data_acquisition.py
+++ data_acquisition.py
@@ -11,3 +11,34 @@
 #     1. https://www.coingecko.com/en/api
 #         a. https://github.com/man-c/pycoingecko
 #     2. https://coinmarketcap.com/api/
+
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+def get_coinmarketcap_data(asset_symbol):
+
+    url = 'https://sandbox-api.coinmarketcap.com/v2/cryptocurrency/market-pairs/latest'
+
+    parameters = {
+    'start':'1',
+    'limit':'5000',
+    'convert':'USD',
+    "symbol": asset_symbol,
+    }
+
+    headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c',
+    }
+
+    session = Session()
+    session.headers.update(headers)
+
+    try:
+        response = session.get(url, params=parameters)
+        data = json.loads(response.text)
+        print(data)
+        return data
+    except (ConnectionError, Timeout, TooManyRedirects) as e:
+        print(e)
