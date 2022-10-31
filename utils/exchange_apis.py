3c1ab698c07acc25f5c1e7f86762765528440bac
Henry Chu <collettemilton@Collettes-MBP.T-mobile.com>
2022-10-30 19:03:24 -0700

    Add exchange api module

---
+++ utils/exchange_apis.py
@@ -0,0 +1,8 @@
+# APIs for data from exchanges
+# https://docs.gemini.com/rest-api/#symbols
+
+# https://api.gemini.com/v1/symbols/details/btcusd
+# https://api.gemini.com/v1/pubticker/:symbol
+# https://api.gemini.com/v1/pricefeed
+
+GEMINI_BASE_URL = 'https://api.gemini.com'
