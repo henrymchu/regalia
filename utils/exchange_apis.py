a527d09daed25808b0f155382316a9692664a86f
Henry Chu <collettemilton@Collettes-MBP.T-mobile.com>
2022-10-30 19:46:10 -0700

    Add get_gemini_asset_price method to exchange_apis

--- utils/exchange_apis.py
+++ utils/exchange_apis.py
@@ -1,4 +1,7 @@
 # APIs for data from exchanges
+
+import requests
+
 # https://docs.gemini.com/rest-api/#symbols
 
 # https://api.gemini.com/v1/symbols/details/btcusd
@@ -6,3 +9,22 @@
 # https://api.gemini.com/v1/pricefeed
 
 GEMINI_BASE_URL = 'https://api.gemini.com'
+
+
+def get_gemini_asset_price(symbol):
+    """Identifies current asset price on Gemini.
+    :arg symbol: string for trading pair
+
+    :returns: dict with bid/ask/latest
+    """
+    url = '{}/v1/publicticker/{}'.format(GEMINI_BASE_URL, 'btcusd')
+    resp = requests.get(url)
+    resp_data = resp.json()
+    ret = {}
+    ret[symbol] = {
+        'bid': resp_data.get('bid'),
+        'ask': resp_data.get('ask'),
+        'latest': resp_data.get('latest')
+    }
+
+    return ret
