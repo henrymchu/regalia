f5bc21c6fb19d82394116eab339ec2061f278b16
Henry Chu <collettemilton@Collettes-MBP.T-mobile.com>
2022-10-31 22:10:10 -0700

    Add Gemini ticker info

---
+++ utils/ticker_constants.py
@@ -0,0 +1,13 @@
+
+
+# Last update 2022-10-31
+KNOWN_GEMINI_ASSETS = [
+    '1INCH', 'AAVE', 'ALCX', 'ALI', 'AMP', 'ANKR', 'APE', 'API3', 'ASH', 'AUDIO', 'AVAX', 'AXS', 'BAL', 'BAT',
+    'BCH', 'BICO', 'BNT', 'BOND', 'BTC', 'BUSD', 'CHZ', 'COMP', 'CRV', 'CTX', 'CUBE', 'CVC', 'DAI', 'DOGE', 'DOT',
+    'DPI', 'EFIL', 'ELON', 'ENJ', 'ENS', 'ERN', 'ETH', 'EUL', 'FET', 'FIDA', 'FIL', 'FRAX', 'FTM', 'FXS', 'GAL',
+    'GALA', 'GFI', 'GMT', 'GRT', 'GUSD', 'IMX', 'INDEX', 'INJ', 'IOTX', 'JAM', 'KNC', 'KP3R', 'LDO', 'LINK', 'LPT',
+    'LQTY', 'LRC', 'LTC', 'LUNA', 'LUSD', 'MANA', 'MASK', 'MATIC', 'MC', 'MCO2', 'METIS', 'MIM', 'MIR', 'MKR', 'MPL',
+    'NMR', 'ORCA', 'OXT', 'PAXG', 'PLA', 'QNT', 'QRDO', 'RAD', 'RARE', 'RAY', 'RBN', 'REN', 'REVV', 'RLY', 'RNDR',
+    'SAMO', 'SAND', 'SBR', 'SHIB', 'SKL', 'SLP', 'SNX', 'SOL', 'SPELL', 'STORJ', 'SUSHI', 'TOKE', 'TRU', 'UMA',
+    'UNI', 'USDC', 'UST', 'WCFG', 'XTZ', 'YFI', 'ZBC', 'ZEC', 'ZRX'
+]
