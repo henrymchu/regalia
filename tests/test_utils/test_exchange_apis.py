4b3f17957f3dc27e1068456e4cb9848a77f5d6ef
Henry Chu <collettemilton@Collettes-MBP.T-mobile.com>
2022-11-19 21:32:10 -0800

    Return float for min ask and max bid in order book helper
      * Also adds convert_ticker_to_symbol tests

--- tests/test_utils/test_exchange_apis.py
+++ tests/test_utils/test_exchange_apis.py
@@ -18,19 +18,38 @@ from utils.exchange_apis import (
 
 class TestExchangeApisMethods(unittest.TestCase):
 
-    def test_convert_ticker_to_symbol(self):
-        # TODO
-        self.assertTrue(True)
+    def test_convert_ticker_to_symbol_binance_us(self):
+        symbol = convert_ticker_to_symbol('BTC', 'binance.us')
+        self.assertEqual(symbol, 'BTCUSD')
+
+    def test_convert_ticker_to_symbol_coinbase(self):
+        symbol = convert_ticker_to_symbol('BTC', 'coinbase')
+        self.assertEqual(symbol, 'BTC-USD')
+
+    def test_convert_ticker_to_symbol_gemini(self):
+        symbol = convert_ticker_to_symbol('BTC', 'gemini')
+        self.assertEqual(symbol, 'btcusd')
+
+    def test_convert_ticker_to_symbol_kraken(self):
+        symbol = convert_ticker_to_symbol('BTC', 'kraken')
+        self.assertEqual(symbol, 'BTCUSD')
+
+    def test_convert_ticker_to_symbol_okcoin(self):
+        symbol = convert_ticker_to_symbol('BTC', 'okcoin')
+        self.assertEqual(symbol, 'BTC-USD')
 
     @patch('requests.get')
     def test_get_gemini_order_book(self, mock_get):
-        # TODO
         mock_get.return_value.status_code = 200
         mock_get.return_value.json.return_value = {
-            'bids': [{'price': '1.10', 'amount': '2.0'}],
-            'asks': [{'price': '1.50', 'amount': '2.0'}]
+            'bids': [{'price': '1.10', 'amount': '2.0'}, {'price': '1.08', 'amount': '2.0'}],
+            'asks': [{'price': '1.50', 'amount': '2.0'}, {'price': '1.54', 'amount': '2.0'}],
         }
-        self.assertTrue(True)
+        order_book_data = get_gemini_order_book('btcusd')
+        self.assertEqual(order_book_data.get('min_ask'), 1.50)
+        self.assertEqual(order_book_data.get('max_bid'), 1.10)
+        self.assertEqual(order_book_data.get('dollar_value_max_bids'), 4.36)
+        self.assertEqual(order_book_data.get('dollar_value_min_asks'), 6.08)
 
     def test_get_okcoin_order_book(self):
         # TODO
